0413adb71aa9a114f9ec6a6c679e0b131158a18a
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-30 12:08:42 +0100

    added new class Prodotto with attributes and methods

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,83 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+    //variabili d'istanza
+    private int codice;  
+    private String nome;  
+    private String marca;
+    private double prezzo;
+    private double iva;
+
+    //metodi
+
+    //costruttore
+    public Prodotto(int codice, String nome, String marca, double prezzo, double iva){
+        this.codice = codice;
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // getter per codice prodotto (accessibile solo in lettura)
+    public int getCodice(){
+        return this.codice;
+    }
+
+    // getter e setter per nome (accessibile sia in lettura che in scrittura)
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    // getter e setter per marca (accessibile sia in lettura che in scrittura)
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    // getter e setter per prezzo (accessibile sia in lettura che in scrittura)
+    public double getPrezzo(){
+        return this.prezzo;
+    }
+
+    public void setPrezzo(double prezzo){
+        this.prezzo = prezzo;
+    }
+
+    // getter e setter per iva (accessibile sia in lettura che in scrittura)
+    public double getIva(){
+        return this.iva;
+    }
+
+    public void setIva(double iva){
+        this.iva = iva;
+    }
+
+    //metodo per avere il prezzo base
+    public double getPrezzoBase(){
+        return this.prezzo;
+    }
+
+    //metodo per avere il prezzo comprensivo di iva formattato
+    public double getPrezzoConIva(){
+        return this.prezzo + (this.prezzo * iva / 100);
+    }
+
+    public static void main(String[] args){
+        Prodotto prodotto = new Prodotto(1234, "Smartphone", "Apple", 1200.00, 20.00);
+
+        System.out.println(prodotto.getPrezzoBase());
+        System.out.println(prodotto.getPrezzoConIva());
+
+    }
+
+
+}
