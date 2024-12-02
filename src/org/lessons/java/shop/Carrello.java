1a52bfa439c0861111267f91e85fd4a4cd7d4f14
claudia13-prog <cappellaclaudia13@gmail.com>
2024-12-02 17:42:00 +0100

    completed main method

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -22,10 +22,30 @@ public class Carrello {
                 System.out.println("Scegli tra Smartphone, Televisore o Cuffie");
                 String prodottoScelto = scanner.nextLine();
 
+                if(prodottoScelto.equals("Smartphone")){
+                    Smartphone smartphone = new Smartphone(124152, "iphone", "Apple", 800.00, 22.0, 24335430, 8);
+                    carrello.add(smartphone);
+
+                }else if(prodottoScelto.equals("Televisore")){
+                    Televisori televisore = new Televisori(5463435, "smart tv", "Samsung", 900.00, 22.00, 50, true);
+                    carrello.add(televisore);
+
+                }else if(prodottoScelto.equals("Cuffie")){
+                    Cuffie cuffie = new Cuffie(65732738, "airpods", "Apple", 200.00, 22.00, "rosa", true);
+                    carrello.add(cuffie);
+
+                }
+
             }else if(scelta.equals("No")){
                 System.out.println("Vai al carrello");
+                
+                for(Prodotto prodotto : carrello){
+                    System.out.println(prodotto.getNome() + " $" + prodotto.getPrezzoConIva());
+                }
                 break;
                 }
+
+
 
         }
 
